8d417ffefe01c30b071455edba454224452d3a4d
Ramulu C <ramulu.chittiboina@gmail.com>
2026-01-27 21:00:43 -0600

    Min Stack Solution

--- examples/src/main/java/com/main/StackMain.java
+++ examples/src/main/java/com/main/StackMain.java
@@ -1,6 +1,7 @@
 package com.main;
 
 import com.arrays.BestTimeToSellAndBuyStocks;
+import com.stack.MinStack;
 import com.stack.ValidParentheses;
 
 public class StackMain {
@@ -14,5 +15,17 @@ public class StackMain {
         // Valid Parentheses problem
         boolean valid = ValidParentheses.isValid2(s);
         System.out.println("Valid Parentheses Results - "+valid);
+
+        // Min Stack Implementation
+        MinStack minStack = new MinStack();
+        minStack.push(1);
+        minStack.push(2);
+        minStack.push(0);
+        minStack.getMin(); // return 0
+        minStack.pop();
+        minStack.top();    // return 2
+        minStack.getMin(); // return 1
+
+        System.out.println("Min Stack Results - "+minStack.getMin());
     }
 }
